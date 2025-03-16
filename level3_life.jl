0a48c36ce9d52d61147ce7bd930d381fcbc93c14
Mihail <brainpowerdash@gmail.com>
2025-03-16 12:45:46 +0300

    сделал игру Жизнь, так и не понял зачем структура и разделение на current/next фреймы

--- level3_life.jl
+++ level3_life.jl
@@ -3,8 +3,24 @@ module GameOfLife
 using Plots
 
 mutable struct Life
-    current_frame::Matrix{Int}
-    next_frame::Matrix{Int}
+    current_frame::Matrix{Int64}
+    next_frame::Matrix{Int64}
+end
+
+function count_neighbors(cf::Matrix{Int64}, x::Int64, y::Int64)
+    count = 0
+    for i in -1:1
+        for j in -1:1
+            if i == 0 && j == 0
+                continue  
+            end
+            nx, ny = x + i, y + j
+            if nx >= 1 && nx <= n && ny >= 1 && ny <= m
+                count += cf[nx, ny]
+            end
+        end
+    end
+    return count
 end
 
 function step!(state::Life)
@@ -15,8 +31,20 @@ function step!(state::Life)
     TODO: вместо случайного шума
     реализовать один шаг алгоритма "Игра жизнь"
     =#
-    for i in 1:length(curr)
-        curr[i] = rand(0:1)
+    for i in 1:n 
+        for j in 1:m 
+            neighbors = count_neighbors(curr, i, j)
+            if curr[i,j] == 1
+                if neighbors < 2 || neighbors > 3 
+                    curr[i,j] = 0 
+                end
+            else
+                if neighbors == 3
+                    curr[i,j] = 1
+                end
+            end
+        end
+
     end
 
     # Подсказка для граничных условий - тор:
@@ -29,10 +57,18 @@ function step!(state::Life)
 end
 
 function (@main)(ARGS)
-    n = 30
-    m = 30
-    init = rand(0:1, n, m)
+    global n = 30
+    global m = 30
+    init = zeros(n, m)
 
+    init[15,15] = 1
+    init[15,16] = 1
+    init[15,14] = 1
+    init[14,15] = 1
+    init[16,14] = 1
+    init[16,15] = 1
+    init[16,16] = 1
+    init[15,17] = 1 #заполнил вручную ради забавы и красивого роста "популяции"
     game = Life(init, zeros(n, m))
 
     anim = @animate for time = 1:100
